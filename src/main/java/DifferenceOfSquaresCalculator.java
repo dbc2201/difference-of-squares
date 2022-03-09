cc4a37cf9a75917683581767e8f9864c109fba82
dbc2201 <divyanshb22@gmail.com>
2022-03-10 00:31:57 +0530

    replace method implementation with functional style code

--- src/main/java/DifferenceOfSquaresCalculator.java
+++ src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,22 +1,13 @@
+import java.util.stream.IntStream;
+
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        // create a variable to store the sum of natural numbers
-        int sum = 0;
-        for (int index = 1; index <= input; index++) {
-            // add the number to the sum
-            sum = sum + index;
-        }
-        return sum * sum; // calculate the square of the total sum of natural numbers
+        return Double.valueOf(Math.pow(IntStream.range(1, input + 1).sum(), 2.0)).intValue();
     }
 
     int computeSumOfSquaresTo(int input) {
-        // create a variable to store the sum of squares
-        int sum = 0;
-        for (int index = 1; index <= input; index++) {
-            sum += index * index; // calculate the square of each natural number and store it to the sum
-        }
-        return sum; // return the total sum of squares of natural numbers
+        return IntStream.range(1, input + 1).map(number -> number * number).sum();
     }
 
     int computeDifferenceOfSquares(int input) {
